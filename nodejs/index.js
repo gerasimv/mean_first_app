f9e33016b608695af7f1d962dc34cfe8cfc5240c
gerasimv <gerasimv@gmail.com>
2022-02-14 20:57:40 +0000

    Заменил имя порта

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { mongoose } = require('./db.js');
-const port = 3030;
+const lPort = 3030;
 
 var kagentController = require('./controllers/kagentController.js');
 
 var app = express();
 app.use(bodyParser.json());
-app.listen(port, () => console.log('Server started on port: ' + port));
+app.listen(lPort, () => console.log('Server started on port: ' + lPort));
 app.use('/kagents', kagentController);
